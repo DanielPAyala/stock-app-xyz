004957a046fc4d3d4bba1d7d857f79b9aef0abfd
Carlos <carlosporrasayala@gmail.com>
2022-08-24 02:38:17 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    
+})
+
+app.listen(4000, () => {
+    console.log("Iniciando en el puerto 4000");
+})
